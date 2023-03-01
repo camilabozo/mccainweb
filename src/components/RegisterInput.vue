de745a6f0cac3fb2759283a0fe8ec89e487d10a8
Camila Bozo <bozo_cami@hotmail.com>
2023-03-01 01:23:14 -0300

    Se integra Firebase y se comienzan pruebas en la base de datos

---
+++ src/components/RegisterInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="register_input">
+    <input type="email" id="email" placeholder="E-mail">
+    <input type="password" id="password" placeholder="Contraseña">
+    <button>Registrarme</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterInput',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.register_input{
+  width: 50%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,26 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQn3eS_4mYtMDnKIcOZc_j5b9tZA7_S-I",
+  authDomain: "mccain-db.firebaseapp.com",
+  projectId: "mccain-db",
+  storageBucket: "mccain-db.appspot.com",
+  messagingSenderId: "217853655346",
+  appId: "1:217853655346:web:bbb87e9be46b3c70973f2b",
+  measurementId: "G-86X3QSJED7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics)
